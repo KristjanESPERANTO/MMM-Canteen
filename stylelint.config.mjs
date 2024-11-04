8f345e899c45587b7065e2d0b951fe6110c2add2
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-11-04 20:23:16 +0100

    Switch to flat config

---
+++ stylelint.config.mjs
@@ -0,0 +1,12 @@
+const config = {
+  "configBasedir": "./",
+  "extends": ["stylelint-config-standard"],
+  "plugins": ["stylelint-prettier"],
+  "root": true,
+  "rules": {
+    "prettier/prettier": true,
+    "selector-class-pattern": null
+  }
+};
+
+export default config;
